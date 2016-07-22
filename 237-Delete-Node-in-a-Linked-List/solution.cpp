617caff70dbd405826b135b891460ca93f5f84ef
Nicksxs <Nicksxs@users.noreply.github.com>
2016-07-22 07:22:44 -0700

    [237. Delete Node in a Linked List][Accepted]committed by LeetCode Extension

--- 237-Delete-Node-in-a-Linked-List/solution.cpp
+++ 237-Delete-Node-in-a-Linked-List/solution.cpp
@@ -10,8 +10,8 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         // if(node->next){
-            (*node)->val = (*(node->next))->val;
-            (*node)->next = (*(node->next))->next;
+            node->val = node->next->val;
+            node->next = node->next->next;
         // }else{
             
         // }
