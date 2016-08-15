73959678a794ed7be015df321365d3504b2a950b
Nicksxs <Nicksxs@users.noreply.github.com>
2016-08-15 22:17:45 +0800

    [137. Single Number II][Accepted]committed by LeetCode Extension

--- 137-Single-Number-II/solution.cpp
+++ 137-Single-Number-II/solution.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
+    // int singleNumber(vector<int>& nums) {
+    //     int ones = 0, twos = 0;
+    //     for(int i = 0; i < nums.size(); i++){
+    //         ones = (ones ^ nums[i]) & ~twos;
+    //         twos = (twos ^ nums[i]) & ~ones;
+    //     }
+    //     return ones;
+    // }
     int singleNumber(vector<int>& nums) {
-        int ones = 0, twos = 0;
-        for(int i = 0; i < nums.size(); i++){
-            ones = (ones ^ nums[i]) & ~twos;
-            twos = (twos ^ nums[i]) & ~ones;
-        }
-        return ones;
-    }
+         vector<int> bit(32,0);
+         int n = nums.size();
+ 
+         for(int i =0; i< n; ++i)
+         {
+             int k=1;
+             for(int j =0; j<32; ++j)
+             {
+                 int rotated;
+                 if((rotated = nums[i]>>j) == 0) break;
+                 bit[j] += rotated & k;
+             }
+         }
+         
+         int target=0;
+         for(int i =0; i<32; ++i)
+         {
+             target += (bit[i]%3 <<i);
+         }
+         return target;
+     }
 };
