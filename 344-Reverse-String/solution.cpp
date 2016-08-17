726ca3eebc8c32ddef24081dc0b0357d261b47a5
Nicksxs <Nicksxs@users.noreply.github.com>
2016-08-18 00:51:45 +0800

    [344. Reverse String][Accepted]committed by LeetCode Extension

--- 344-Reverse-String/solution.cpp
+++ 344-Reverse-String/solution.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     string reverseString(string s) {
         int len = s.length();
-        string *str = new string(len, '0');
-        for(int i = n - 1; i > 0; i--){
-            str[n-i-1] = s[i];
-        }
-        return *str;
+	    string str(len, '0');
+	    for (int i = (len - 1); i >= 0; i--) {
+			str[len - i - 1] = s[i];
+	    }
+        return str;
     }
 };
