04a75ffb58c717f63dd886da5ebc70b3887a05bf
Nicksxs <Nicksxs@users.noreply.github.com>
2016-08-18 00:26:46 +0800

    [344. Reverse String][Compile Error]committed by LeetCode Extension

--- 344-Reverse-String/solution.cpp
+++ 344-Reverse-String/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string reverseString(string s) {
         int len = s.length();
-        string *str = new string(len);
+        string *str = new string(len, '0');
         for(int i = n - 1; i > 0; i--){
             str[n-i-1] = s[i];
         }
