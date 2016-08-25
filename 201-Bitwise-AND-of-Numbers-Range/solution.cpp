7f0475d94420400fdf09e7b3a2a6231f648b50a6
Nicksxs <Nicksxs@users.noreply.github.com>
2016-08-25 23:37:11 +0800

    [201. Bitwise AND of Numbers Range][Accepted]committed by LeetCode Extension

--- 201-Bitwise-AND-of-Numbers-Range/solution.cpp
+++ 201-Bitwise-AND-of-Numbers-Range/solution.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-        int d = INT_MAX;
-        while ((m & d) != (n & d)) {
-            d <<= 1;
+        // method 1
+        // int d = INT_MAX;
+        // while ((m & d) != (n & d)) {
+        //     d <<= 1;
+        // }
+        // return m & d;
+        
+        // method 2
+        int i = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            ++i;
         }
-        return m & d;
+        return (m << i);
     }
 };
