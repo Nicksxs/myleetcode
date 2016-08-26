9beaad3b1eed23ab8c183c23f808a77636b4fcb1
Nicksxs <Nicksxs@users.noreply.github.com>
2016-08-27 00:26:23 +0800

    [278. First Bad Version][Accepted]committed by LeetCode Extension

--- 278-First-Bad-Version/solution.cpp
+++ 278-First-Bad-Version/solution.cpp
@@ -4,9 +4,12 @@ bool isBadVersion(int version);
 class Solution {
 public:
     int firstBadVersion(int n) {
-        while(!isBadVersion(n-1)){
-            n = n / 2;
+        int lower = 1, upper = n, mid;
+        while(lower < upper) {
+            mid = lower + (upper - lower) / 2;
+            if(!isBadVersion(mid)) lower = mid + 1;   /* Only one call to API */
+            else upper = mid;
         }
-        return n;
+        return lower;   /* Because there will alway be a bad version, return lower here */
     }
 };
