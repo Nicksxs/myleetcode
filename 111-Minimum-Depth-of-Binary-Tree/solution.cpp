5e567c331979fcce4007de98623555f85ebe82e9
Nicksxs <Nicksxs@users.noreply.github.com>
2016-09-07 22:40:38 +0800

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.cpp
+++ 111-Minimum-Depth-of-Binary-Tree/solution.cpp
@@ -10,9 +10,26 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        if(!root->left) return 1 + minDepth(root->right);
-        if(!root->right) return 1 + minDepth(root->left);
-        return 1 + min(minDepth(root->left),minDepth(root->right));
+        // if(!root) return 0;
+        // if(!root->left) return 1 + minDepth(root->right);
+        // if(!root->right) return 1 + minDepth(root->left);
+        // return 1 + min(minDepth(root->left),minDepth(root->right));
+        // Start typing your C/C++ solution below  
+      // DO NOT write int main() function  
+      if(root == NULL)  
+        return 0;  
+      int lmin = minDepth(root->left);  
+      int rmin = minDepth(root->right);  
+      if(lmin ==0 && rmin ==0)  
+        return 1;  
+      if(lmin ==0)  
+      {  
+        lmin = INT_MAX;  
+      }  
+      if(rmin ==0)  
+      {  
+        rmin = INT_MAX;  
+      }  
+      return min(lmin, rmin) +1;  
     }
 };
