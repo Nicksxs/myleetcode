046c2c5f9bbd6f053bbd2e436a7e2387c2677894
Nicksxs <Nicksxs@users.noreply.github.com>
2016-09-12 22:58:51 +0800

    [382. Linked List Random Node][Compile Error]committed by LeetCode Extension

---
+++ 382-Linked-List-Random-Node/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int ans = 0;
+		ListNode *p = this->head;
+		for (int cnt = 1; p; cnt++, p = p->next) if (rand() % cnt == 0) ans = p->val;
+		return ans;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
